da3673be07eec1f597557ffdd49f29ee9ef5ef5b
AmalinaZakiyahSalsabilaAribowo <amalina_zakiyah_27rpl@student.smktelkom-mlg.sch.id>
2020-11-27 22:32:09 +0700

    added

---
+++ array_1.js
@@ -0,0 +1,27 @@
+// array string
+let jurusan = ["Rekayasa Perangkat Lunak", "Teknik Komputer dan Jaringan"]
+
+// arrau numerik
+let tingkatan_kelas = [10,11,12]
+
+// array objek
+let siswa = [
+  //objek pertama
+  {
+    nama: "Yaya", jurusan: "RPL"
+  },
+
+  //objek kedua
+  {
+    nama: "Ying", jurusan: "TKJ"
+  },
+
+  //objek ketiga
+  {
+    nama: "Gopal", jurusan: "RPL"
+  }
+]
+
+console.log("Jumlah elemen array jurusan = " + jurusan.length);
+console.log("Jumlah elemen array tingkatan_kelas = " + tingkatan_kelas.length);
+console.log("Jumlah elemen array siswa = " + siswa.length);
