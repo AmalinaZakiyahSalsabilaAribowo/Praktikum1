7df90e9b4618f78584cc7257d17c1b0096cbc54f
AmalinaZakiyahSalsabilaAribowo <amalina_zakiyah_27rpl@student.smktelkom-mlg.sch.id>
2020-11-27 22:36:39 +0700

    added2

---
+++ array_4.js
@@ -0,0 +1,15 @@
+let nama =
+[
+  "Ana", "Sulaiman", "Putri", "Gatot", "Adit", "Gico"
+]
+
+console.log("Isi array nama");
+console.log(nama);
+console.log("Jumlah data saat ini = " + nama.length);
+
+//hapus data
+nama.splice(2,1);
+
+console.log("Isi array nama");
+console.log(nama);
+console.log("Jumlah data saat ini = " + nama.length);
